a1fa0f7d700529f027636b3b23c7b94337dceaaa
aranj05 <emailid>
2024-04-26 07:25:07 +0000

    tests

--- src/test/java/parallelScripts/SampleThree.java
+++ src/test/java/parallelScripts/SampleThree.java
@@ -6,18 +6,18 @@ public class SampleThree {
 	@Test
 	  public void testOne() {
 		long id= Thread.currentThread().getId();
-		  System.out.println("This is TestOne- SampleThree" +id);
+		  System.out.println("This is TestOne- SampleThree.." +id);
 	  }
 	  
 	  @Test
 	  public void testTwo() {
 		  long id= Thread.currentThread().getId();
-		  System.out.println("This is TestTwo- SampleThree" +id);
+		  System.out.println("This is TestTwo- SampleThree.." +id);
 	  }
 	  
 	  @Test
 	  public void testThree() {
 		  long id= Thread.currentThread().getId();
-		  System.out.println("This is TestThree- SampleThree" +id);
+		  System.out.println("This is TestThree- SampleThree.." +id);
 	  }
 }
