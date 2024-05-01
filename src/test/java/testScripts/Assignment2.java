317c9888de602a0b3539ea0111010fa20c44634d
aranj05 <emailId>
2024-05-01 06:01:13 +0000

    assignment

--- src/test/java/testScripts/Assignment2.java
+++ src/test/java/testScripts/Assignment2.java
@@ -1,10 +1,8 @@
 package testScripts;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Properties;
 
 import org.json.simple.JSONArray;
@@ -13,18 +11,15 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
-public class ExternalFileTest {
+public class Assignment2 {
 	WebDriver driver;
 	Properties prop;
 	
@@ -41,7 +36,6 @@ public class ExternalFileTest {
 	@BeforeMethod
 	public void setup() {
 		String strBrowser=prop.getProperty("browser");
-		System.out.println("Browser name.."+strBrowser);
 		if(strBrowser.equalsIgnoreCase("chrome")) {
 			driver=new ChromeDriver();
 		}
@@ -51,60 +45,34 @@ public class ExternalFileTest {
 		driver.manage().window().maximize();
 	}
 	
-	
   @Test (dataProvider="logindata")
-  public void ValidLogin(String strUser, String strPwd) {
-	  driver.get(prop.getProperty("url"));          //url= https://the-internet.herokuapp.com/login 
-		driver.findElement(By.id("username")).sendKeys(strUser);
-		driver.findElement(By.name("password")).sendKeys(strPwd);
-		driver.findElement(By.className("radius")).click();
-		boolean isValid=driver.findElement(By.cssSelector("div#flash-messages")).isDisplayed();
-		Assert.assertTrue(isValid);
+  public void addItem() {
+	  driver.get(prop.getProperty("url"));
+	  driver.findElement(By.id("login2")).click();
+	  //driver.findElement(By.id("loginusername")).sendKeys(prop.getProperty("username"));
+	  //driver.findElement(By.id("loginpassword")).sendKeys(prop.getProperty("password"));
+	  driver.findElement(By.className("btn btn-primary")).click();
   }
   
-  /*@DataProvider (name="logindata")
-  public Object[][] getData(){
-	  String path= System.getProperty("user.dir")+
-			  "//src//main//resources//testData//loginData.csv";
-	  CSVReader reader=null;
-	  
-	  try {
-		  reader=new CSVReader(new FileReader(path));
-	  }
-	  
-	  catch (FileNotFoundException e) {
-		  e.printStackTrace();
-	  }
-	  String cols[];
-	  ArrayList<Object> dataList=new ArrayList<Object>();
-	  
-	  try {
-		  while((cols=reader.readNext())!= null) {
-			  Object record[]= {cols[0], cols[1]};
-			  dataList.add(record);
-		  }
-		  reader.close();
-	  }
-	  catch (CsvValidationException | IOException e) {
-		  e.printStackTrace();
-	  }
-	 return dataList.toArray(new Object[dataList.size()][]); 
-  }*/
   @DataProvider (name="logindata")
   public Object[][] getData() throws IOException, ParseException{
 	  String path= System.getProperty("user.dir")+
-			  "//src//main//resources//testData//loginData.json";
+			  "//src//main//resources//testData//customerDetails.json";
 	  FileReader reader=new FileReader(path);
 	  JSONParser parser=new JSONParser();
 	  Object obj= parser.parse(reader);
 	  JSONObject jsonObj=(JSONObject)obj;
-	  JSONArray userArray= (JSONArray)jsonObj.get("userLogins");
+	  JSONArray userArray= (JSONArray)jsonObj.get("customerData");
 	  String arr[][]=new String[userArray.size()][];
 	  for(int i=0; i<userArray.size();i++) {
 		  JSONObject user=(JSONObject)userArray.get(i);
-		  String strUser= (String)user.get("username");
-		  String strPwd= (String)user.get("password");
-		  String record[]= {strUser,strPwd};
+		  String strName= (String)user.get("Name");
+		  String country= (String)user.get("Country");
+		  String city= (String)user.get("City");
+		  String creditCard= (String)user.get("Credit card");
+		  String month= (String)user.get("Month");
+		  String year= (String)user.get("Year");
+		  String record[]= {strName,country,city,creditCard,month,year};
 		  arr[i]=record;
 	  }
 	  return arr;
